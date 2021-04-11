66fa2d8921eacf1216507ef6f88177fa9622f30c
omarzer0 <omarzero1015@gmail.com>
2021-04-11 19:54:51 +0200

    mi card app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
@@ -14,35 +16,75 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('images/gusion.png'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.yellow,
+              Text(
+                'Gusion',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'ANDROID DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    fontFamily: 'SansPro',
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
                   ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.yellow.withOpacity(0.5),
-                  )
-                ],
+                  title: Text(
+                    '+20 100 488 1647',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SansPro',
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
               ),
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.blue,
-              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'omarzero1015@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SansPro',
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
